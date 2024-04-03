6204ffde94e77aa58aee2880b80c9475b7240458
Matheus Mello <matheusgmello1@gmail.com>
2024-04-03 11:41:17 -0300

    feat: endpoints para criação e detalhes do evento

--- src/main/java/br/com/matheusgmello/passin/controllers/EventController.java
+++ src/main/java/br/com/matheusgmello/passin/controllers/EventController.java
@@ -1,13 +1,14 @@
 package br.com.matheusgmello.passin.controllers;
 
+import br.com.matheusgmello.passin.dto.event.EventIdDTO;
+import br.com.matheusgmello.passin.dto.event.EventRequestDTO;
+import br.com.matheusgmello.passin.dto.event.EventResponseDTO;
 import br.com.matheusgmello.passin.services.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/events")
@@ -17,8 +18,18 @@ public class EventController {
     private final EventService service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> getEvent(@PathVariable String id){
-        this.service.getEventDetail(id);
-        return ResponseEntity.ok("success");
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id){
+        EventResponseDTO event = this.service.getEventDetail(id);
+        return ResponseEntity.ok(event);
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDTO eventIdDTO = this.service.createEvent(body);
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+        return ResponseEntity.created(uri).body(eventIdDTO);
+
     }
 }
